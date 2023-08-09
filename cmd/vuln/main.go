d1f94614c1eb4a5bbdbb937b2b06008513728563
Hardie Boeve <hboeve@pax8.com>
2023-08-09 20:35:04 +0200

    insecure code

---
+++ cmd/vuln/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	resp, err := http.Get("https://example.com")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(body))
+}
