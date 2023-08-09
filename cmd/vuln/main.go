dba2ca01748957f46a6d4b476e670e662c3c513a
Hardie Boeve <hboeve@pax8.com>
2023-08-09 20:40:21 +0200

    insecure code

--- cmd/vuln/main.go
+++ cmd/vuln/main.go
@@ -8,7 +8,12 @@ import (
 )
 
 func main() {
-	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{
+		CipherSuites: []uint16{
+			tls.TLS_RSA_WITH_RC4_128_SHA, // weak cipher suite.
+		},
+		MinVersion: tls.VersionTLS10, // Older TLS versions have known vulnerabilities.
+	}
 
 	resp, err := http.Get("https://example.com")
 	if err != nil {
