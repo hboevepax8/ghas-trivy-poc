be859948cce73d87055c5fe9ae3c3ae8b46af4a1
Hardie Boeve <hboeve@pax8.com>
2023-08-09 21:16:09 +0200

    expose secrets

---
+++ cmd/s3/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+const (
+	AWS_ACCESS_KEY_ID     = "YOUR_ACCESS_KEY_ID"
+	AWS_SECRET_ACCESS_KEY = "YOUR_SECRET_ACCESS_KEY"
+)
+
+func main() {
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("us-north-1"),
+		Credentials: credentials.NewStaticCredentials(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, ""),
+	})
+
+	if err != nil {
+		fmt.Println("Error creating session:", err)
+		return
+	}
+
+	s3Svc := s3.New(sess)
+	result, err := s3Svc.ListBuckets(nil)
+	if err != nil {
+		fmt.Println("Error listing buckets:", err)
+		return
+	}
+
+	fmt.Println("Buckets:")
+	for _, bucket := range result.Buckets {
+		fmt.Println(*bucket.Name)
+	}
+}
